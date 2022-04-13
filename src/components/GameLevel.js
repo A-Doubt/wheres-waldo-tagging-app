47fd475a5316cbc3a1682e9f87a0243fe902aba9
A-Doubt <adoubt2@gmail.com>
2022-04-13 20:27:44 +0200

    Added popup on win. Submits data to firebase backend.

--- src/components/GameLevel.js
+++ src/components/GameLevel.js
@@ -1,6 +1,11 @@
 import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
 import CharToFind from './CharToFind';
 import Popup from './Popup';
+import { db } from '../index';
+import { collection, addDoc } from 'firebase/firestore';
+import { nanoid } from 'nanoid'
+import { useNavigate } from "react-router-dom";
 
 export default function GameLevel(props) {
 	const [popupVisible, setPopupVisible] = React.useState(false);
@@ -14,7 +19,22 @@ export default function GameLevel(props) {
 	const [isWin, setIsWin] = React.useState(false);
 	const [gameLength, setGameLength] = React.useState(null);
 	const [gratzPopupVisible, setGratzPopupVisible] = React.useState(false);
+	const [nameInput, setNameInput] = React.useState({ value: '' });
 
+	let navigate = useNavigate();
+
+	// FIREBASE CODE SECTION
+	// console.log('DATABASE: ', db);
+	async function postScore(entry) {
+		try {
+			const docRef = await addDoc(collection(db, 'score'), entry);
+			console.log('Document written with ID: ', docRef.id);
+		} catch (e) {
+			console.error('Error adding document: ', e);
+		}
+	}
+
+	//****************************************************************
 	function tag(e) {
 		setCoords({ x: e.pageX, y: e.pageY });
 
@@ -35,29 +55,35 @@ export default function GameLevel(props) {
 		setPopupVisible(false);
 
 		if (
-			value === 'waldo' && 
-			(props.waldo.x1 < tileClicked.x && tileClicked.x < props.waldo.x2) && 
-			(props.waldo.y1 < tileClicked.y && tileClicked.y < props.waldo.y2)
+			value === 'waldo' &&
+			props.waldo.x1 < tileClicked.x &&
+			tileClicked.x < props.waldo.x2 &&
+			props.waldo.y1 < tileClicked.y &&
+			tileClicked.y < props.waldo.y2
 		) {
 			setIsFound((prevIsFound) => {
-				return ({...prevIsFound, waldo: true})
-			})
-		}
-		else if (value === 'odlaw' && 
-			(props.odlaw.x1 < tileClicked.x && tileClicked.x < props.odlaw.x2) && 
-			(props.odlaw.y1 < tileClicked.y && tileClicked.y < props.odlaw.y2)
+				return { ...prevIsFound, waldo: true };
+			});
+		} else if (
+			value === 'odlaw' &&
+			props.odlaw.x1 < tileClicked.x &&
+			tileClicked.x < props.odlaw.x2 &&
+			props.odlaw.y1 < tileClicked.y &&
+			tileClicked.y < props.odlaw.y2
 		) {
 			setIsFound((prevIsFound) => {
-				return ({...prevIsFound, odlaw: true})
-			})
-		}
-		else if (value === 'wizard' && 
-			(props.wizard.x1 < tileClicked.x && tileClicked.x < props.wizard.x2) && 
-			(props.wizard.y1 < tileClicked.y && tileClicked.y < props.wizard.y2)
+				return { ...prevIsFound, odlaw: true };
+			});
+		} else if (
+			value === 'wizard' &&
+			props.wizard.x1 < tileClicked.x &&
+			tileClicked.x < props.wizard.x2 &&
+			props.wizard.y1 < tileClicked.y &&
+			tileClicked.y < props.wizard.y2
 		) {
 			setIsFound((prevIsFound) => {
-				return ({...prevIsFound, wizard: true})
-			})
+				return { ...prevIsFound, wizard: true };
+			});
 		}
 	}
 
@@ -73,21 +99,85 @@ export default function GameLevel(props) {
 			setIsWin(true);
 			setGratzPopupVisible(true);
 		}
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [isFound]);
 
 	React.useEffect(() => {
-		setGameLength(parseFloat(((gameEnd.current - gameStart.current) / 1000).toFixed(2)));
+		setGameLength(
+			parseFloat(
+				((gameEnd.current - gameStart.current) / 1000).toFixed(2)
+			)
+		);
 
 		console.log(gameLength);
-	}, [isWin, gameLength])
+	}, [isWin, gameLength]);
+
+	function handleInputChange(e) {
+		setNameInput({ value: e.target.value });
+	}
+
+	function handleAddScore(name, scenery, length) {
+
+		const newEntry = {
+			id: nanoid(),
+			name: name,
+			scenery: scenery,
+			length: length,
+		}
+		console.log(newEntry);
+		postScore(newEntry);
+		navigate("/leaderboards");
+	}
 
 	return (
 		<>
 			{gratzPopupVisible && (
 				<>
-					<div className="game--win-popup">Congratulations!</div>
-					<p>You found all characters in {gameLength}s!</p>
+					<div className="game--win-popup">
+						<div className="game--win-form">
+							<p className="gratz-message">Congratulations!</p>
+							<p className="gratz-message">
+								you finished in:
+								<span className="game-time">
+									{' '}
+									{gameLength}s!
+								</span>
+							</p>
+							<label htmlFor="username">
+								Enter your name: (max 20 characters)
+							</label>
+							<div className="flex-row">
+								<input
+									type="text"
+									maxLength="20"
+									id="username"
+									autoComplete="off"
+									placeholder="name"
+									onChange={handleInputChange}
+									value={nameInput.value}
+								></input>
+								<button
+									className="red-btn add"
+									onClick={() =>
+										handleAddScore(
+											nameInput.value,
+											props.scenery,
+											gameLength
+										)
+									}
+								>
+									Add score
+								</button>
+							</div>
+							<div className="flex-row">
+								<Link to="/leaderboards">
+									<button className="red-btn">
+										View Leaderboards
+									</button>
+								</Link>
+							</div>
+						</div>
+					</div>
 				</>
 			)}
 			<div className="game--page">
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -25,6 +25,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+const db = getFirestore(app);
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
+
+export { db }; 
